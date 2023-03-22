b30e367e844b67f685f5207621c703f53ac6a9af
JN Zhang <zhangjianan1229@163.com>
2023-03-22 23:01:27 +0800

    first commit

---
+++ src/components/RuoYi/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JMallDoc',
+  data() {
+    return {
+      url: 'http://doc.jmall.vip/jmall-cloud'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
---
+++ src/components/RuoYi/Git/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JMallGit',
+  data() {
+    return {
+      url: 'https://gitee.com/y_project/JMall-Cloud'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
